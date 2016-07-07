aa8bc36b402ba257780f3dda1f7e61183d8545de
Andy Shapiro <shapiromatron@gmail.com>
2016-07-07 16:16:52 -0400

    code cleanup

--- project/assets/AnalysisVisual/IndividualOverview.js
+++ project/assets/AnalysisVisual/IndividualOverview.js
@@ -9,35 +9,37 @@ class IndividualOverview {
     constructor(el, analysis_id) {
         this.el = el;
         this.id = window.analysisObjectID;
+    }
 
-        this.individualOverviewInitURL = function(id) {
-            return (`/dashboard/api/analysis/${id}/individual_overview/`);
-        };
+    individualOverviewInitURL(id) {
+        return `/dashboard/api/analysis/${id}/individual_overview/`;
+    }
 
-        this.dscFullRowValueURL = function(id, row_name) {
-            return (
-                `/dashboard/api/`+
-                `analysis/${id}/`+
-                `dsc_full_row_value/`+
-                `?row=${row_name}`
-            );
-        };
+    dscFullRowValueURL(id, row_name) {
+        return `/dashboard/api/analysis/${id}/dsc_full_row_value/?row=${row_name}`;
     }
 
     renderSelectList() {
-        function addOptions(el, option_array) {
-            var select = el.find('#select_list');
-
-            select.empty();
-
-            d3.select(select.get(0))
-                .selectAll('option')
-                .data(option_array)
-                .enter()
-                .append('option')
-                .text(function(d) {return d;})
-                .attr('value', function(d) {return d;});
-        }
+        var selectable,
+            el = this.el,
+            addOptions = function(el, option_array) {
+                var select = el.find('#select_list');
+                select.empty()
+                    .html(option_array.map((d)=>{
+                        return `<option value="${d}">${d}</option>`;
+                    }).join(''));
+            },
+            showMatches = function() {
+                let value = this.value.toLowerCase();
+                if (value === '') {
+                    selectable = window.matrix_names;
+                } else {
+                    selectable = $.grep(window.matrix_names, function(n) {
+                        return (n.toLowerCase().includes(value));
+                    });
+                }
+                addOptions(el, selectable);
+            };
 
         //Remove heatmap div if there; append heatmap div
         this.el.find('#select_list').remove();
@@ -59,218 +61,199 @@ class IndividualOverview {
 
         select_list[0].selectedIndex = 0;
 
-        var selectable;
-        var el = this.el;
-
         this.el.find('#search_field').remove();
 
         $('<input>')
             .attr({
-                'type': 'text',
-                'id': 'search_field',
-                'placeholder': 'Filter data list',
-                'outerWidth': '30%',
-                'outerHeight': '15%',
+                type: 'text',
+                id: 'search_field',
+                placeholder: 'Filter data list',
+                outerWidth: '30%',
+                outerHeight: '15%',
             }).css({
-                'position': 'absolute',
-                'left': '0%',
-                'top': '0%',
-                'width': '30%',
-                'height': '15%',
-                'overflow': 'scroll',
+                position: 'absolute',
+                left: '0%',
+                top: '0%',
+                width: '30%',
+                height: '15%',
+                overflow: 'scroll',
             })
-            .on('input', function() {
-                let value = this.value.toLowerCase();
-                if (value === '') {
-                    selectable = window.matrix_names;
-                } else {
-                    selectable = $.grep(window.matrix_names, function(n) {
-                        return (n.toLowerCase().includes(value));
-                    });
-                }
-                addOptions(el, selectable);
-            })
+            .on('input', showMatches)
             .appendTo(el);
         this.displayCorrelations();
-        // });
     }
 
-    addDisplayButtons() {
+    addDisplayButton() {
         this.el.find('#display_heatmap').remove();
         $('<button>Display individual heatmap</button>')
             .attr({
-                'type': 'button',
-                'class': 'btn btn-primary',
+                type: 'button',
+                class: 'btn btn-primary',
             }).css({
-                'position': 'absolute',
-                'left': '0%',
-                'top': '80%',
-                'width': '30%',
+                position: 'absolute',
+                left: '0%',
+                top: '80%',
+                width: '30%',
             }).click(
                 this.displayIndividualHeatmap.bind(this)
             ).appendTo(this.el);
     }
 
-    displayCorrelations() {
+    renderCorrelations(selected, row_data){
+        var num = row_data.length - 1,
+            entry_length = 6,
+            margin = {top: 0, right: 0, bottom: 20, left: 0},
+            offset = {top: 20, right: 0, bottom: 100, left: 40},
+            width = (num*entry_length > this.el.find('#correlation_plot').width())
+                ? (num*entry_length - margin.left - margin.right)
+                : (this.el.find('#correlation_plot').width() - margin.left - margin.right),
+            height = this.el.find('#correlation_plot').height();
+
+        var sortable = [];
+        for (let i = 0; i < window.col_names.length; i++) {
+            if (selected !== window.col_names[i]) {
+                sortable.push([window.col_names[i], row_data[i]]);
+            }
+        }
+
+        if (window.sort_vector == null){
+            sortable.sort(function(a, b) {return Math.abs(b[1]) - Math.abs(a[1]);});
+        }
+
+        var $graph = $('<div id="graph">')
+            .css({
+                'height': height,
+                'width': width,
+                'position': 'absolute',
+                'left': '0%',
+                'top': margin.top,
+            }).appendTo(this.el.find('#correlation_plot'));
+
+        var graph = d3.select($graph.get(0)).append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g');
+
+        var x = d3.scale.ordinal()
+            .domain(sortable.map(function(d) {return d[0];}))
+            .rangeBands([6,width - offset.left - offset.right]);
+
+        var y = d3.scale.linear()
+            .domain([-1,1])
+            .range([height - offset.top - offset.bottom,0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient('bottom')
+            .outerTickSize(0);
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient('left')
+            .ticks(5);
+
+        var handleMouseOver = function (d) {
+            $(this).tooltip({
+                container: 'body',
+                title: `${d[0]}<br/>${d[1].toFixed(2)}`,
+                html: true,
+                animation: false,
+            }).tooltip('show');
+        };
+
+        var lines = [-1, 0, 1];
+
+        graph.append('g')
+            .selectAll('rect')
+            .data(sortable)
+            .enter()
+            .append('rect')
+            .style('fill', (d) => {return (d[1] >= 0)? 'red': 'blue';})
+            .attr('x', (d) => offset.left + x(d[0]))
+            .attr('width', x.rangeBand() - 2)
+            .attr('y', (d) => offset.top + y(Math.max(0,d[1])))
+            .attr('height', (d) => Math.abs(y(0) - y(d[1])));
+
+        graph.append('g')
+            .selectAll('rect')
+            .data(sortable)
+            .enter()
+            .append('rect')
+            .style('fill', 'transparent')
+            .attr('x', (d) => offset.left + x(d[0]))
+            .attr('width', x.rangeBand() - 2)
+            .attr('y', offset.top)
+            .attr('height', y(-1))
+            .on('mouseover', handleMouseOver);
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        graph.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', `translate(${offset.left},${height - offset.bottom})`)
+            .call(xAxis)
+            .style('fill', 'none')
+            .selectAll('text')
+            .style('fill', 'black')
+            .attr('font-size', '8px')
+            .style('text-anchor', 'end')
+            .attr('transform', 'rotate(-90)' )
+            .attr('dx', '-8px')
+            .attr('dy', '-8px');
+
+        graph.append('g')
+            .attr('class', 'y axis')
+            .attr('transform', `translate(${offset.left},${offset.top})`)
+            .style('fill', 'none')
+            .style('stroke', 'black')
+            .style('stroke-width', '1px')
+            .call(yAxis)
+            .selectAll('text')
+            .attr('font-size','8px')
+            .style('fill', 'black')
+            .style('stroke', 'none');
+
+        graph.append('g')
+            .selectAll('line')
+            .data(lines)
+            .enter()
+            .append('line')
+            .attr('x1', offset.left)
+            .attr('x2', width - offset.right)
+            .attr('y1', (d) => offset.top + y(d))
+            .attr('y2', (d) => offset.top + y(d))
+            .style('stroke', 'black')
+            .style('stroke-width', 1)
+            .style('stroke-dasharray', (d) => {return (d===0)? 'none': '5,5';});
+    }
+
+    displayCorrelations(){
         this.el.find('#correlation_plot').remove();
         $('<div id="correlation_plot">')
             .css({
-                'height': '100%',
-                'width': '68%',
-                'position': 'absolute',
-                'left': '32%',
-                'top': '0%',
-                'overflow': 'scroll',
+                height: '100%',
+                width: '68%',
+                position: 'absolute',
+                left: '32%',
+                top: '0%',
+                overflow: 'scroll',
             }).appendTo(this.el);
 
-        var selected = this.el.find('#select_list').find('option:selected').text();
+        var selected = this.el.find('#select_list').find('option:selected').text(),
+            url = this.dscFullRowValueURL(this.id, selected);
 
-        $.get(this.dscFullRowValueURL(this.id, selected), function(row_data) {
-            var num = row_data.length - 1,
-                entry_length = 6,
-                margin = {top: 0, right: 0, bottom: 20, left: 0},
-                offset = {top: 20, right: 0, bottom: 100, left: 40},
-                width = (num*entry_length > this.el.find('#correlation_plot').width())
-                    ? (num*entry_length - margin.left - margin.right)
-                    : (this.el.find('#correlation_plot').width() - margin.left - margin.right),
-                height = this.el.find('#correlation_plot').height();
-
-                $('<div id="graph">')
-                    .css({
-                        'height': height,
-                        'width': width,
-                        'position': 'absolute',
-                        'left': '0%',
-                        'top': margin.top,
-                    }).appendTo(this.el.find('#correlation_plot'));
-
-                var sortable = [];
-                for (let i = 0; i < window.col_names.length; i++) {
-                    if (selected !== window.col_names[i]) {
-                        sortable.push([window.col_names[i], row_data[i]]);
-                    }
-                }
-
-                if (window.sort_vector == null){
-                    sortable.sort(function(a, b) {return Math.abs(b[1]) - Math.abs(a[1]);});
-                }
-
-                var y = d3.scale.linear()
-                    .domain([-1,1])
-                    .range([height - offset.top - offset.bottom,0]);
-                var x = d3.scale.ordinal()
-                    .domain(sortable.map(function(d) {return d[0];}))
-                    .rangeBands([6,width - offset.left - offset.right]);
-
-                var graph = d3.select(this.el.find('#graph').get(0)).append('svg')
-                    .attr('width', width)
-                    .attr('height', height)
-                    .append('g');
-
-                var xAxis = d3.svg.axis()
-                    .scale(x)
-                    .orient('bottom')
-                    .outerTickSize(0);
-
-                var yAxis = d3.svg.axis()
-                    .scale(y)
-                    .orient('left')
-                    .ticks(5);
-
-                graph.append('g')
-                    .selectAll('rect')
-                    .data(sortable)
-                    .enter()
-                    .append('rect')
-                    .style('fill', function(d) { if (d[1] >= 0) { return 'red'; } else { return 'blue'; }})
-                    .attr('x', function(d) { return offset.left + x(d[0]); })
-                    .attr('width', x.rangeBand() - 2)
-                    .attr('y', function(d) { return offset.top + y(Math.max(0,d[1])); })
-                    .attr('height', function(d) { return Math.abs(y(0) - y(d[1])); });
-
-                graph.append('g')
-                    .selectAll('rect')
-                    .data(sortable)
-                    .enter()
-                    .append('rect')
-                    .style('fill', 'transparent')
-                    .attr('x', function(d) { return offset.left + x(d[0]); })
-                    .attr('width', x.rangeBand() - 2)
-                    .attr('y', offset.top)
-                    .attr('height', y(-1))
-                    .on('mouseover', function (d) {
-                        //d3.select(this).style('stroke', 'black').style('stroke-width', '1');
-                        var content = (d[0] + '<br/>' + d[1].toFixed(2));
-                        $(this).tooltip({
-                            container: 'body',
-                            title: content,
-                            html: true,
-                            animation: false,
-                        });
-                        $(this).tooltip('show');
-                    });
-                    /*
-                    .on('mouseout', function () {
-                        d3.select(this).style('stroke', 'none');
-                    });
-                    */
-
-                $('[data-toggle="tooltip"]').tooltip();
-
-                graph.append('g')
-                    .attr('class', 'x axis')
-                    .attr('transform', 'translate(' + offset.left + ',' + (height - offset.bottom) + ')')
-                    .call(xAxis)
-                    .style('fill', 'none')
-                    .selectAll('text')
-                    .style('fill', 'black')
-                    .attr('font-size', '8px')
-                    .style('text-anchor', 'end')
-                    .attr('transform', 'rotate(-90)' )
-                    .attr('dx', '-8px')
-                    .attr('dy', '-8px');
-
-                graph.append('g')
-                    .attr('class', 'y axis')
-                    .attr('transform', 'translate(' + offset.left + ',' + offset.top +')')
-                    .style('fill', 'none')
-                    .style('stroke', 'black')
-                    .style('stroke-width', '1px')
-                    .call(yAxis)
-                    .selectAll('text')
-                    .attr('font-size','8px')
-                    .style('fill', 'black')
-                    .style('stroke', 'none');
-
-                var lines = [-1, 0, 1];
-
-                graph.append('g')
-                    .selectAll('line')
-                    .data(lines)
-                    .enter()
-                    .append('line')
-                    .attr('x1', offset.left)
-                    .attr('x2', width - offset.right)
-                    .attr('y1', function(d) { return offset.top + y(d); })
-                    .attr('y2', function(d) { return offset.top + y(d); })
-                    .style('stroke', 'black')
-                    .style('stroke-width', 1)
-                    .style('stroke-dasharray', function (d) {if (d === 0) {return 'none';} else {return '5,5';}});
-        }.bind(this));
+        $.get(url, this.renderCorrelations.bind(this, selected));
     }
 
     displayIndividualHeatmap () {
-        var name = this.el.find('#select_list').find('option:selected').text();
-
-        var modalTitle = $('#ind_heatmap_modal_title'),
-            modalBody = $('#ind_heatmap_modal_body');
-
-        $('#flcModal')
-            .one('show.bs.modal', function(){
+        var name = this.el.find('#select_list').find('option:selected').text(),
+            modalTitle = $('#ind_heatmap_modal_title'),
+            modalBody = $('#ind_heatmap_modal_body'),
+            onModalShowing = function(){
                 modalTitle.html('');
                 modalBody.html('');
-            })
-            .one('shown.bs.modal', function(){
+            },
+            onModalShown = function(){
                 var individual_heatmap = new IndividualHeatmap(
                     window.name_to_id[name],
                     window.matrix_names,
@@ -278,22 +261,30 @@ class IndividualOverview {
                     name,
                     modalTitle,
                     modalBody,
-                    window.sort_vector,
+                    window.sort_vector
                 );
                 individual_heatmap.render();
-            }).modal('show');
+            };
+
+        $('#flcModal')
+            .one('show.bs.modal', onModalShowing)
+            .one('shown.bs.modal', onModalShown)
+            .modal('show');
     }
 
     render() {
-        $.get(this.individualOverviewInitURL(this.id), function(data) {
-            window.col_names = data['col_names'],
+        var url = this.individualOverviewInitURL(this.id),
+            cb = function(data) {
+                window.col_names = data['col_names'],
                 window.matrix_names = data['matrix_names'],
                 window.matrix_ids = data['matrix_IDs'],
                 window.sort_vector = data['sort_vector'];
-            window.name_to_id = _.object(data['matrix_names'], data['matrix_IDs']);
-            this.renderSelectList();
-            this.addDisplayButtons();
-        }.bind(this));
+                window.name_to_id = _.object(data['matrix_names'], data['matrix_IDs']);
+                this.renderSelectList();
+                this.addDisplayButton();
+            };
+
+        $.get(url, cb.bind(this));
     }
 
 }
