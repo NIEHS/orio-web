a6f87411ae4279dcbdf83f47d3bc048de8aba547
Andy Shapiro <shapiromatron@gmail.com>
2016-07-07 14:49:02 -0400

    use less jQuery for main setup

--- project/assets/AnalysisVisual/main.js
+++ project/assets/AnalysisVisual/main.js
@@ -8,71 +8,40 @@ import FeatureClusteringOverview from './FeatureClusteringOverview';
 
 let startup = function(){
     $(document).ready(function(){
-        // set environment variables to window
+
+        // set configuration variables to window
         let config = JSON.parse(document.getElementById('config').textContent);
         _.each(config, (v, k) => window[k] = v);
 
         // create instances for visualization
-        let overview = new AnalysisOverview($('#visual_panel_1')),
-            individual_overview = new IndividualOverview($('#visual_panel_2')),
-            feature_clust_overview = new FeatureClusteringOverview(
-                $('#visual_panel_1'), $('#visual_panel_2'));
+        let pnl1 = $('#visual_panel_1'),
+            pnl2 = $('#visual_panel_2'),
+            btnDc = $('#data_clust_button'),
+            btnFc = $('#feature_clust_button'),
+            overview = new AnalysisOverview(pnl1),
+            individual_overview = new IndividualOverview(pnl2),
+            feature_clust_overview = new FeatureClusteringOverview(pnl1, pnl2),
+            activeClass = {class: 'btn btn-primary'},
+            inactiveClass = {class: 'btn btn-default'},
+            handleFeatureClusterClick = function() {
+                btnFc.attr(activeClass);
+                btnDc.attr(inactiveClass);
+                pnl1.empty();
+                pnl2.empty();
+                feature_clust_overview.render();
+            },
+            handleDataClusterClick = function() {
+                btnFc.attr(inactiveClass);
+                btnDc.attr(activeClass);
+                pnl1.empty();
+                pnl2.empty();
+                overview.render();
+                individual_overview.render();
+            };
 
-        // add buttons for visualization selection
-        $('<button>Data set clustering</button>')
-            .attr({
-                'type': 'button',
-                'id': 'data_clust_button',
-                'class': 'btn btn-primary',
-            }).css({
-                'position': 'absolute',
-                'left': '0%',
-                'top': '0%',
-                'width': '30%',
-            }).appendTo('#analysis_selection_panel');
-        $('<button>Feature clustering</button>')
-            .attr({
-                'type': 'button',
-                'id': 'feature_clust_button',
-                'class': 'btn btn-default',
-            }).css({
-                'position': 'absolute',
-                'left': '31%',
-                'top': '0%',
-                'width': '30%',
-            }).appendTo('#analysis_selection_panel');
-
-        $('#data_clust_button').click( function() {
-            $('#data_clust_button').attr({
-                'class': 'btn btn-primary',
-            });
-            $('#feature_clust_button').attr({
-                'class': 'btn btn-default',
-            });
-
-            $('#visual_panel_1').empty();
-            $('#visual_panel_2').empty();
-
-            overview.render();
-            individual_overview.render();
-        });
-
-        $('#feature_clust_button').click( function() {
-            $('#feature_clust_button').attr({
-                'class': 'btn btn-primary',
-            });
-            $('#data_clust_button').attr({
-                'class': 'btn btn-default',
-            });
-
-            $('#visual_panel_1').empty();
-            $('#visual_panel_2').empty();
-
-            feature_clust_overview.render();
-        });
-
-        overview.render();
-        individual_overview.render();
+        // setup button handlers
+        btnDc.click(handleDataClusterClick).trigger('click');
+        btnFc.click(handleFeatureClusterClick);
     });
 };
 
