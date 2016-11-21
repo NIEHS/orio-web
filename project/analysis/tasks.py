fdf5bac7f2717a55e65ba142676aa8fcabf4d20b
Andy Shapiro <shapiromatron@gmail.com>
2016-11-21 14:47:18 -0500

    fixup celery tasks and use a shortcut to reduce boilerplate

--- project/analysis/tasks.py
+++ project/analysis/tasks.py
@@ -10,6 +10,11 @@ from django.core.exceptions import ObjectDoesNotExist
 logger = get_task_logger(__name__)
 
 
+def gm(name):
+    # shortcut to get model from analysis application
+    return apps.get_model('analysis', name)
+
+
 @task()
 def raise_error():
     """DEBUG task added to ensure that celery errors are handled correctly."""
@@ -19,8 +24,12 @@ def raise_error():
 @task(bind=True)
 def execute_analysis(self, analysis_id, silent):
     """Run all feature-list count matrix in parallel."""
-    EncodeDataset = apps.get_model('analysis', 'EncodeDataset')
-    analysis = apps.get_model('analysis', 'Analysis').objects.get(id=analysis_id)
+    try:
+        analysis = gm('Analysis').objects.get(id=analysis_id)
+    except ObjectDoesNotExist:
+        return
+
+    EncodeDataset = gm('EncodeDataset')
     ads_qs = analysis.analysisdatasets_set.all()\
         .prefetch_related('dataset', 'dataset__encodedataset', 'dataset__userdataset')
     task1 = group([
@@ -42,33 +51,26 @@ def execute_analysis(self, analysis_id, silent):
 @task()
 def execute_count_matrix(analysis_id, ads_id, is_encode, dataset_id):
     """Execute each count matrix."""
-
     try:
-        analysis = apps.get_model('analysis', 'Analysis').objects.get(id=analysis_id)
-        ads = apps.get_model('analysis', 'AnalysisDatasets').objects.get(id=ads_id)
+        analysis = gm('Analysis').objects.get(id=analysis_id)
+        ads = gm('AnalysisDatasets').objects.get(id=ads_id)
         if is_encode:
-            dataset = apps.get_model('analysis', 'EncodeDataset').objects.get(id=dataset_id)
+            dataset = gm('EncodeDataset').objects.get(id=dataset_id)
         else:
-            dataset = apps.get_model('analysis', 'UserDataset').objects.get(id=dataset_id)
+            dataset = gm('UserDataset').objects.get(id=dataset_id)
     except ObjectDoesNotExist:
         return
 
-    FeatureListCountMatrix = apps.get_model('analysis', 'FeatureListCountMatrix')
+    FeatureListCountMatrix = gm('FeatureListCountMatrix')
     ads.count_matrix = FeatureListCountMatrix.execute(analysis, dataset)
-
-    try:
-        apps.get_model('analysis', 'Analysis').objects.get(id=analysis_id)
-    except ObjectDoesNotExist:
-        return
-    else:
-        ads.save()
+    ads.save()
 
 
 @task()
 def execute_matrix_combination(analysis_id, silent):
     """Save results from matrix combination."""
     try:
-        analysis = apps.get_model('analysis', 'Analysis').objects.get(id=analysis_id)
+        analysis = gm('Analysis').objects.get(id=analysis_id)
     except ObjectDoesNotExist:
         return
     analysis.output = analysis.execute_mat2mat()
@@ -80,17 +82,16 @@ def execute_matrix_combination(analysis_id, silent):
 
 @task()
 def download_dataset(id_):
-    dd = apps.get_model('analysis', 'DatasetDownload').objects.get(id=id_)
+    dd = gm('DatasetDownload').objects.get(id=id_)
     dd.download()
 
 
 @task()
 def analysis_zip(id_):
-    analysis = apps.get_model('analysis', 'Analysis').objects.get(id=id_)
+    analysis = gm('Analysis').objects.get(id=id_)
     analysis.create_zip()
 
 
 @periodic_task(run_every=timedelta(hours=1))
 def remove_expired_download_links():
-    TemporaryDownload = apps.get_model('analysis', 'TemporaryDownload')
-    TemporaryDownload.remove_expired()
+    gm('TemporaryDownload').remove_expired()
